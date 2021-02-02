f19bb4e4135289a795443f419c3ec0e56f79c9ce
Anthony Steel <13as261@queensu.ca>
2021-02-02 12:34:27 -0500

    Find depth of a leaf and traverse preorder

--- bst.lisp
+++ bst.lisp
@@ -1,24 +1,3 @@
-(defparameter tree1 `(8 (3 1 (6 4 7)) (10 nil (14 13 nil))))
-;;(defparameter tree2 `(8 (3 (1 nil nil) (6 (4 nil nil) (7 nil nil)) (10 nil (14 13 nil)))))
-;;(defparameter tree3 `(6 (3 1 5) (9 7 11)))
-
-(defun bst-depth (tree)
-  (cond ((null tree) 0)
-        ((atom tree) 1)
-        (t (let ((left-subtree (second tree))
-                 (right-subtree (third tree)))
-             (+ 1 (max (bst-depth left-subtree)
-                       (bst-depth right-subtree)))))))
-
-(defun bst-depth-and-value (lst tree depth)
-  (if (atom tree)
-      (append lst `((depth tree)))
-      (let ((left-subtree (second tree))
-            (right-subtree (third tree)))
-        (progn (append lst `((depth (first tree))))
-               (bst-depth-and-value (lst left-subtree (+ 1 depth)))
-               (bst-depth-and-value (lst right-subtree (+ 1 depth)))))))
-
 (defun power (x n)
   (cond ((= n 0) 1)
         (t (* x (power x (- n 1))))))
@@ -29,25 +8,38 @@
         (t (+ (count-anywhere x (first lst))
               (count-anywhere x (rest lst))))))
 
-(defun bst-pretty-print (tree)
-  ;; Consider the bst
-  ;; (8 (3 1 (6 4 7)) (10 nil (14 13 nil)))
-  ;; Visually,
-  ;;                   8
-  ;;                /     \
-  ;;               3      10
-  ;;              / \       \
-  ;;             1   6      14
-  ;;                / \    /
-  ;;               4   7  13
-  ;; First print 8
-  ;; then 3 and 10
-  ;; then 1 6 and 14
-  ;;
-  ;; Create a list where each element in the list is a list of
-  ;; two elements, the depth and the value. Example:
-  ;; ((1 8) (2 3) (2 10) (3 1) (3 6) (3 14) (4 4) (4 7) (4 13))
-  (bst-depth-and-value (nil tree 0)))
+(defparameter tree1 `(8 (3 1 (6 4 7)) (10 nil (14 13 nil))))
+(defparameter tree2 `(8 (3 (1 nil nil) (6 (4 nil nil) (7 nil nil)) (10 nil (14 13 nil)))))
+(defparameter tree3 `(6 (3 1 5) (9 7 11)))
+(defparameter tree4 `(38 (5 1 (9 8 (15 13 nil))) (45 nil (47 46 nil))))
+
+
+(defun bst-depth (tree)
+  (cond ((null tree) 0)
+        ((atom tree) 1)
+        (t (let ((left-subtree (second tree))
+                 (right-subtree (third tree)))
+             (+ 1 (max (bst-depth left-subtree)
+                       (bst-depth right-subtree)))))))
+
+(defun bst-preorder (tree)
+  (cond ((null tree) nil)
+        ((atom tree) (list tree))
+        (t (append (bst-preorder (first tree))
+                   (bst-preorder (rest tree))))))
+
+(defun bst-find (value tree)
+  (cond ((null tree) nil)
+        ((atom tree) (= tree value))
+        (t (or (bst-find value (first tree))
+               (bst-find value (rest tree))))))
+
+(defun bst-find-depth (depth value tree)
+  (cond ((null tree) nil)
+        ((atom tree) (and (= tree value) depth))
+        (t (or (bst-find-depth depth value (first tree))
+               (bst-find-depth (+ 1 depth) value (second tree))
+               (bst-find-depth (+ 1 depth) value (third tree))))))
 
 
 ;; Tests
