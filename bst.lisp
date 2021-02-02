57f9e9e5eddab9816d94e6938bfc07b761707636
Anthony Steel <13as261@queensu.ca>
2021-02-02 12:44:52 -0500

    Remove bst from function names

--- bst.lisp
+++ bst.lisp
@@ -14,45 +14,50 @@
 (defparameter tree4 `(38 (5 1 (9 8 (15 13 nil))) (45 nil (47 46 nil))))
 
 
-(defun bst-depth (tree)
+(defun depth (tree)
+  ;; Find the maximum depth of the tree
   (cond ((null tree) 0)
         ((atom tree) 1)
         (t (let ((left-subtree (second tree))
                  (right-subtree (third tree)))
-             (+ 1 (max (bst-depth left-subtree)
-                       (bst-depth right-subtree)))))))
+             (+ 1 (max (depth left-subtree)
+                       (depth right-subtree)))))))
 
-(defun bst-preorder (tree)
+(defun preorder (tree)
+  ;; Return the tree as a list in preorder
   (cond ((null tree) nil)
         ((atom tree) (list tree))
-        (t (append (bst-preorder (first tree))
-                   (bst-preorder (rest tree))))))
+        (t (append (preorder (first tree))
+                   (preorder (rest tree))))))
 
-(defun bst-find (value tree)
+(defun find (value tree)
+  ;; Find if a value exists in a tree
   (cond ((null tree) nil)
         ((atom tree) (= tree value))
-        (t (or (bst-find value (first tree))
-               (bst-find value (rest tree))))))
+        (t (or (find value (first tree))
+               (find value (rest tree))))))
 
-(defun bst-find-depth (depth value tree)
+(defun find-depth (depth value tree)
+  ;; Find the depth of a node if the value exists in the tree
   (cond ((null tree) nil)
         ((atom tree) (and (= tree value) depth))
-        (t (or (bst-find-depth depth value (first tree))
-               (bst-find-depth (+ 1 depth) value (second tree))
-               (bst-find-depth (+ 1 depth) value (third tree))))))
-
+        (t (or (find-depth depth value (first tree))
+               (find-depth (+ 1 depth) value (second tree))
+               (find-depth (+ 1 depth) value (third tree))))))
+(defun preorder-with-depth (tree)
+  (let (preorder-tree preorder(tree))))
 
 ;; Tests
-(defun test-bst-depth ()
+(defun test-depth ()
   (let ((tree0 `(8))
         (tree1 `(8 (3 1 (6 4 7)) (10 nil (14 13 nil))))
         (tree2 `(8 (3 (1 nil nil) (6 (4 nil nil) (7 nil nil)) (10 nil (14 13 nil)))))
         (tree3 `(6 (3 1 5) (9 7 11)))
         (tree4 `(8 (3 (1 nil nil) (6 4 nil)) 10)))
-    (if (and (= (bst-depth tree0) 1)
-             (= (bst-depth tree1) 4)
-             (= (bst-depth tree2) 4)
-             (= (bst-depth tree3) 3)
-             (= (bst-depth tree4) 4))
+    (if (and (= (depth tree0) 1)
+             (= (depth tree1) 4)
+             (= (depth tree2) 4)
+             (= (depth tree3) 3)
+             (= (depth tree4) 4))
         t
         nil)))
