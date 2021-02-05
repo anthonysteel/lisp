c25c768434eb3298f28f4c40f82e492c6a74ea33
Anthony Steel <13as261@queensu.ca>
2021-02-04 20:51:22 -0500

    Add vertices of cube

--- cad.lisp
+++ cad.lisp
@@ -1,4 +1,35 @@
+(ql:quickload `(:qtools :qtcore :qtgui))
+
+(defpackage #:cad
+  (:use #:cl+qt)
+  (:export #:main))
+(in-package :cad)
+(in-readtable :qtools)
+
+(define-widget main-window (Qwidget) ())
+
+(define-subwidget (main-window go-button)
+    (q+:make-qpushbutton "Go!" main-window))
+
+(define-subwidget (main-window layout) (q+:make-qhboxlayout main-window)
+  (q+:add-widget layout name)
+  (q+:add-widget layout go-button))
+
+(with-main-window
+    (window `main-window))
+
 (defparameter *name* "bearing-cap")
 
 (defun name-stl (name)
   (concatenate `string "solid " name))
+
+
+(defun cube-vertices ()
+  `((0 0 0)
+    (0 0 1)
+    (0 1 0)
+    (0 1 1)
+    (1 0 0)
+    (1 0 1)
+    (1 1 0)
+    (1 1 1)))
