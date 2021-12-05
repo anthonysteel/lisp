6948cdc3a1b1578bf1f3917d15669cf851941fa0
Anthony Steel <13as261@queensu.ca>
2021-12-05 10:55:49 -0500

    Solve problem 3.1 in SICP

--- sicp.scm
+++ sicp.scm
@@ -5,3 +5,13 @@
     (lambda (num)
       (begin (set! acc (+ acc num))
 	     acc))))
+
+;; 3.2 make-monitored
+(define (make-monitored f)
+  (let ((i 0) (fun f))
+    (define (mf m)
+      (cond ((eq? m 'how-many-calls?) i)
+	    ((eq? m 'reset-count) (begin (set! i 0) i))
+	    ((number? m) (begin (set! i (+ i 1)) (fun m)))
+	    (else (error "Unknown request -- MAKE-MONITORED" m))))
+    mf))
